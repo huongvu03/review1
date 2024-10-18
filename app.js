9b8b95098d16caa78500e125bb97403a08096ec2
huongvu03 <huongvu031096@gmail.com>
2024-10-18 15:02:59 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var booksRouter = require('./routes/book');
+
+const mongoose = require("mongoose");
+//mongoose.connect("mongodb://localhost:27017/day8DB")
+mongoose.connect("mongodb+srv://huongvu:123@hdb1.4wtpp.mongodb.net/day8DB?retryWrites=true&w=majority&appName=HDB1")
+.then(() => console.log("Connected to MongoDB"))
+.catch(err=> console.log(err));
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/book', booksRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ models/book.model.js
@@ -0,0 +1,9 @@
+const { default: mongoose } = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  publication_year: Number,
+  genre: String,
+})
+module.exports = mongoose.model("books",  bookSchema);
